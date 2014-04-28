780ba3b8f1271dd0a4b78c7a3bb00f5f4fabf2eb
David Boucha <boucha@gmail.com>
2014-04-27 23:48:27 -0600

    Add requirement for opt in

--- external_ip.py
+++ external_ip.py
@@ -8,13 +8,21 @@
     salt.grains.external_ip
     ~~~~~~~~~~~~~~~~~~~~~~~
 
-    Return the external IP address reported by boucha.saltstack.com
+    Return the external IP address reported by an IP reporting service.
+    SaltStack provides this at http://boucha.saltstack.com:8080
+
+    The following config item is mandatory to opt in
+
+        external_ip.server: 'http://boucha.saltstack.com:8080'
+
+    The following minion config items are optional:
+
+        external_ip.timeout: 0.5
 '''
 
 # Import Python Libs
 import logging
 import requests
-from pprint import pprint
 
 # Import salt libs
 from salt.utils.validate.net import ipv4_addr as _ipv4_addr
@@ -28,11 +36,11 @@ def external_ip():
     '''
     Return the external IP address
     '''
-    check_url = 'http://boucha.saltstack.com:8080'
-
-    if __opts__.get('request_external_ip', False):
+    check_url = __opts__.get('external_ip.server', False)
+    if check_url: 
         try:
-            r = requests.get(check_url, timeout=0.1)
+            timeout = __opts__.get('external_ip.timeout', 0.5)
+            r = requests.get(check_url, timeout=timeout)
             ip_addr = r.json()
             return {'external_ip': ip_addr['ip_addr']}
         except Timeout as exc:
